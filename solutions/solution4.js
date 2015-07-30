9ddc64037fd86a977df06b98d8dd2d8de0bf22d7
Samuel Cabral <samfcabral@gmail.com>
2015-07-29 19:09:50 -0700

    add solution4

--- solutions/solution4.js
+++ solutions/solution4.js
@@ -1,3 +1,8 @@
 var string = "I am happy to be here today";
-var newString = string.replace(/ /g, "%20");
-console.log(newString);
+
+var replaceSpace = function (str) {
+  var newString = string.replace(/ /g, "%20");
+  console.log(newString);
+};
+
+replaceSpace(string);

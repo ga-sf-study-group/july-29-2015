5d6cc10b3b44c1ae641d07a4718ad17a35855fab
Jane Philipps <jane.philipps@gmail.com>
2015-07-30 10:04:55 -0700

    update solution4

--- solutions/solution4.js
+++ solutions/solution4.js
@@ -4,3 +4,26 @@ var replaceSpace = function (str) {
 };
 
 replaceSpace("I am happy to be here today");
+
+// Without JS replace method
+var replaceSpaces = function (s) {
+    // create empty array to push into
+    var newS = [];
+    // iterate over string
+    for (var i = 0; i < s.length; i++) {
+        // check for chars in string
+        if (s[i] !== " ") {
+            // push chars into empty array
+            newS.push(s[i]);
+        } else {
+            // for each white space, push %20 into array
+            newS.push("%20");
+        }
+    }
+     // return array newS as string using join
+    return newS.join("");
+};
+
+replaceSpaces(s);
+replaceSpaces("a b c");
+replaceSpaces("  test  ");
